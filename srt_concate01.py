21d4098104981f0c40e51444082c7f423426beaf
Renyuan Lyu <renyuan.lyu@gmail.com>
2023-07-31 09:38:33 +0800

    ....

--- srt_concate01.py
+++ srt_concate01.py
@@ -23,11 +23,15 @@ fList+= glob.glob(f'{mp4Path}/*/*/*/*.srt')
 # using \n\n to separate each file
 
 numCh= 0
+nL= []
+tL= []
 with (open(f'_{mp4Path}_.srt.txt', 'w', encoding='utf-8') as f,
       open(f'_{mp4Path}_.srt0.txt', 'w', encoding='utf-8') as f0):
     for n,i in tqdm.tqdm(enumerate(fList)):
         with open(i, 'r', encoding='utf-8') as f2:
             filename= os.path.basename(i)
+            nL += [(n, filename)]
+            
             # extract the code enclosed in square bracket '[]'of filename
             # e.g.
             # input: "112年7月阿彌陀佛紐涅七日禪-0000-112年7月阿彌陀佛紐涅七日禪(第1集) [FaCLg4H0BU0].srt"
@@ -40,6 +44,17 @@ with (open(f'_{mp4Path}_.srt.txt', 'w', encoding='utf-8') as f,
             # remove the all time stamps in txtS
             #txtS0= '\n'.join([i for i in txtS.split('\n') if not i.startswith('00:')])
             txtS0= '\n'.join([i for i in txtS.split('\n') if ' --> ' not in i])
+
+            # get the last time stamp in txtS
+            lastTS= txtS.split('\n')[-4].split(' --> ')[-1]
+            #print(f'{lastTS= }')
+            lastTS= lastTS.split(':')
+            hr= int(lastTS[0])
+            min= int(lastTS[1])
+            sec= int(lastTS[2].split(',')[0]) + int(lastTS[2].split(',')[1])*.001
+            t = hr*3600 + min*60 + sec
+            tL += [t]
+
 
             f.write(f'file{n:04d}: {os.path.basename(i)}\n')
             f.write(f'url{n:04d}: {url}\n')
@@ -56,8 +71,20 @@ with (open(f'_{mp4Path}_.srt.txt', 'w', encoding='utf-8') as f,
             numCh += len([i for i in txtS if '\u4e00' <= i <= '\u9fff'])
  
 
+#print(f'{nL= }')
+for n,f in nL:
+    print(f'{n= }, {f= }')
 
-print(f'{numCh= }')
+print(f'{len(nL)= } files')
+
+#print(f'{tL= }')
+#print(f'{sum(tL)= :.3f} seconds')
+print(f'{sum(tL)/3600= :.3f} hours')
+
+#print(f'{numCh= } chars')
+# print numCh using large number format
+print(f'{numCh= :,} chars')
+
 
 
 
